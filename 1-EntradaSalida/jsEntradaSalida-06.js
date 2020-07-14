f5f9a3c81449b37b21a65773ee94c789d7b4d865
joaquincahais <joaco.cahais@gmail.com>
2020-07-14 12:27:34 -0300

    Ejercicios 6 al 10

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,20 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var numero1;
+	var numero2;
+	
+	numero1 = txtIdNumeroUno.value;
+	numero2 = txtIdNumeroDos.value;
+
+	numero1 = parseInt(numero1);
+	numero2 = parseInt(numero2);
+
+
+	alert(numero1 + numero2);
+
+	
+
+	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,16 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var aumento;
+	var resultado;
+	
+	sueldo = txtIdSueldo.value;
+	sueldo = parseInt(sueldo);
 
-	alert("ok");
+	aumento = (10*sueldo)/100;
+	
+	resultado = sueldo + aumento;
+	txtIdResultado.value = resultado;
+	
 }
